b346ce8b7a646776fdefc4287f7f398b445ca083
angela.lu <angela.lu@budweiserapac.com>
2024-07-03 18:06:45 +0800

    feat: 完成课程6

--- 06-text-generation-apps/python/aoai-app.py
+++ 06-text-generation-apps/python/aoai-app.py
@@ -1,3 +1,11 @@
+'''
+Author: angela.lu “angela.lu@budweiserapac.com”
+Date: 2024-06-25 13:36:21
+LastEditors: angela.lu “angela.lu@budweiserapac.com”
+LastEditTime: 2024-07-03 14:25:55
+FilePath: /generative-ai-for-beginners/06-text-generation-apps/python/aoai-app.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from openai import AzureOpenAI
 import os
 import dotenv
@@ -8,7 +16,7 @@ dotenv.load_dotenv()
 # configure Azure OpenAI service client 
 client = AzureOpenAI(
   azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
-  api_key=os.environ['AZURE_OPENAI_KEY'],  
+  api_key=os.environ['AZURE_OPENAI_API_KEY'],  
   api_version = "2023-10-01-preview"
   )
 
--- 06-text-generation-apps/typescript/recipe-app/src/main.ts
+++ 06-text-generation-apps/typescript/recipe-app/src/main.ts
@@ -1,16 +1,39 @@
-import { OpenAIClient, AzureKeyCredential, ChatRequestMessage } from "@azure/openai";
+/*
+ * @Author: angela.lu “angela.lu@budweiserapac.com”
+ * @Date: 2024-06-25 13:36:21
+ * @LastEditors: angela.lu “angela.lu@budweiserapac.com”
+ * @LastEditTime: 2024-07-03 17:45:43
+ * @FilePath: /generative-ai-for-beginners/06-text-generation-apps/typescript/recipe-app/src/main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { OpenAIClient, AzureKeyCredential, ChatRequestMessageUnion } from "@azure/openai";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT || '';
-const azureApiKey = process.env.AZURE_OPENAI_KEY || '';
+const azureApiKey = process.env.AZURE_OPENAI_API_KEY || '';
 
 export async function main() {
     console.log("== Recipe Recommendation App ==");
 
+    /**
+     * Parameters
+        endpoint
+        string
+
+        The URI for an Azure OpenAI resource, including protocol and hostname. For example: https://my-resource.openai.azure.com.
+
+        credential
+        KeyCredential
+        A key credential used to authenticate to an Azure OpenAI resource.
+
+        options
+        OpenAIClientOptions
+        The options for configuring the client.
+     */
     const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-    const deploymentName = 'deployment-name-completion';
+    const deploymentName = process.env.AZURE_OPENAI_DEPLOYMENT || '';
     //const deploymentName = '<include-your-deployment-name-here>';
 
     console.log("Number of recipes: (for example: 5): ");
@@ -24,7 +47,7 @@ export async function main() {
 
     const promptText = `Show me ${numRecipes} recipes for a dish with the following ingredients: ${ingredients}. Per recipe, list all the ingredients used, no ${filter}: `;
 
-    const chatMessages: ChatRequestMessage[] = [
+    const chatMessages: ChatRequestMessageUnion[] = [
         {
             role: 'system',
             content: 'Hello, I am a recipe recommendation bot. I will recommend recipes based on the ingredients you provide me.'
@@ -49,7 +72,7 @@ export async function main() {
 
         const newPrompt = `Given ingredients at home: ${ingredients} and these generated recipes: ${oldPromptResult}, ${promptShoppingList}`;
 
-        const shoppingListMessages: ChatRequestMessage[] = [
+        const shoppingListMessages: ChatRequestMessageUnion[] = [
             {
                 role: 'system',
                 content: 'Here is your shopping list:'
